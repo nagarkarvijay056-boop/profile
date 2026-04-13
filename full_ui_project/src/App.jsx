3b28ef9710e915227b21e55f6778d975ebef1839
VJ0803 <nagarkarvijay056@gmail.com>
2026-04-13 19:58:00 +0530

    Delete full_ui_project directory

--- full_ui_project/src/App.jsx
+++
@@ -1,9 +0,0 @@
-import { Button } from "./components/ui/button"
-export default function App() {
-  return (
-    <div className="p-10">
-      <h1 className="text-2xl font-bold">UI Working</h1>
-      <Button>Click me</Button>
-    </div>
-  )
-}
--- full_ui_project/src/components/ui/button.jsx
+++
@@ -1,4 +0,0 @@
-import * as React from "react"
-export function Button({ children }) {
-  return <button className="px-4 py-2 bg-black text-white rounded">{children}</button>
-}
